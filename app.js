1aadbcc8b5d4975439288fb0a0ef6908f32e9095
PhucDLHSE <danglehoangphuc2003@gmail.com>
2025-03-11 14:37:31 +0700

    debug app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
@@ -22,10 +21,6 @@ app.use('/api/categories', require('./src/routes/categoryRoutes'));
 app.use('/api/product-types', require('./src/routes/productTypeRoutes'));
 app.use('/api/products', require('./src/routes/productRoutes'));
 app.use('/api/exchange-notes', require('./src/routes/exchangeNoteRoutes'));
-app.use("/", require('./src/routes/indexRoutes'));
-
-app.use("/favicon.ico", express.static(path.join(__dirname, "public", "favicon.ico")));
-app.get("/favicon.ico", (req, res) => res.status(204).end());
 
 // Middleware log request
 app.use((req, res, next) => {
