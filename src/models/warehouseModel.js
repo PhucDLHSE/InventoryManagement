48e6cd47cbf2a588cd7edbd5f81d78c1dd18f015
PhucDLHSE <danglehoangphuc2003@gmail.com>
2025-03-27 01:48:19 +0700

    fix warehouse api

--- src/models/warehouseModel.js
+++ src/models/warehouseModel.js
@@ -225,41 +225,40 @@ class Warehouse {
         
         const [products] = await pool.query(`
           SELECT 
-    p.product_code,
-    p.product_name,
-    p.size,
-    p.color,
-    pt.productType_name,
-    pt.productType_code,
-    COALESCE(
-        (SELECT SUM(
-            CASE 
-                WHEN (e.transactionType = 'IMPORT' AND e.destination_warehouse_code = ?) THEN ni.quantity
-                WHEN (e.transactionType = 'EXPORT' AND e.source_warehouse_code = ?) THEN -ni.quantity
-                ELSE 0
-            END
-        )
-        FROM NoteItem ni
-        JOIN ExchangeNote e ON ni.exchangeNote_id = e.exchangeNote_id
-        WHERE ni.product_code = p.product_code
-        AND e.status = 'finished'
-        ), 0
-    ) AS quantity_in_warehouse,
-    p.status
-FROM 
-    Product p
-JOIN 
-    ProductType pt ON p.productType_code = pt.productType_code
-WHERE EXISTS (
-    SELECT 1 FROM NoteItem ni
-    JOIN ExchangeNote e ON ni.exchangeNote_id = e.exchangeNote_id
-    WHERE ni.product_code = p.product_code
-    AND (e.destination_warehouse_code = ? OR e.source_warehouse_code = ?)
-    AND e.status = 'finished'
-)
-HAVING quantity_in_warehouse > 0
-ORDER BY p.product_name;
-
+            p.product_code,
+            p.product_name,
+            p.size,
+            p.color,
+            pt.productType_name,
+            pt.productType_code,
+            COALESCE(
+              (SELECT SUM(
+                CASE 
+                  WHEN (e.transactionType = 'IMPORT' AND e.destination_warehouse_code = ?) THEN ni.quantity
+                  WHEN (e.transactionType = 'EXPORT' AND e.source_warehouse_code = ?) THEN -ni.quantity
+                  ELSE 0
+                END
+              )
+              FROM NoteItem ni
+              JOIN ExchangeNote e ON ni.exchangeNote_id = e.exchangeNote_id
+              WHERE ni.product_code = p.product_code
+              AND e.status = 'finished'
+              ), 0
+            ) as quantity_in_warehouse,
+            p.status
+          FROM 
+            Product p
+          JOIN 
+            ProductType pt ON p.productType_code = pt.productType_code
+          WHERE EXISTS (
+            SELECT 1 FROM NoteItem ni
+            JOIN ExchangeNote e ON ni.exchangeNote_id = e.exchangeNote_id
+            WHERE ni.product_code = p.product_code
+            AND (e.destination_warehouse_code = ? OR e.source_warehouse_code = ?)
+            AND e.status = 'finished'
+          )
+          HAVING quantity_in_warehouse > 0
+          ORDER BY p.product_name
         `, [warehouse_code, warehouse_code, warehouse_code, warehouse_code, warehouse_code, warehouse_code]);
         
         return products;
